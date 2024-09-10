2ae9ef97f67526be14eaaf0a79344b543528d7e1
marleneanton <the.marlene.anton@gmail.com>
2024-09-10 11:14:30 +0200

    Initial commit

---
+++ src/main/java/org/example/databasepractice/DatabasePracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.databasepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabasePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/databasepractice/DatabasePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.databasepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
